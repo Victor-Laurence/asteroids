91740352d6f1d4f82c6f3c7aa84d9470c3a60e1e
Victor Laurence <victor@victorlaurence.com>
2024-10-30 11:46:34 -0700

    Player object and rendering it on screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -10,13 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
-        pygame.display.flip()
+
+        player.draw(screen)
+
+        pygame.display.flip()        
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
